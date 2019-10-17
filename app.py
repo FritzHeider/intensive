d10ab2294d1da741bb6265a6e4b0294080963059
FritzHeider <fritz.heider@students.makeschool.com>
2019-10-17 13:22:37 -0700

    making major progress

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.TinyHomeMe
+users = db.users
+
 
 app = Flask(__name__)
 
+users = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+def users_index():
+    """Show all users."""
+    return render_template('users_index.html', users=users.find())
+
+@app.route('/')
+def home():
+    """Show all users."""
+    return render_template('home.html', msg='Flask is Cool!!' )
+
+
 
 
 if __name__ == '__main__':
